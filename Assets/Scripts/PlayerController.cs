cc34e5c996c6493282863c8fbfd15758d0d85e1c
Trawirr <gtrawirr@gmail.com>
2023-10-16 09:57:06 +0200

    Player controller updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,51 @@ public class PLayerController : MonoBehaviour
     [Space(10)]
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] public LayerMask groundLayer;
-    [SerializeField] public float rayLength = 2.0f;
+    [SerializeField] public float rayLength = 2.0f; 
+    [SerializeField] public float horizontalRayOffset = 0.55f;
     [Space(10)]
     [SerializeField] private Animator animator;
     private bool isWalking = false;
+    private bool isFalling = false;
+    private bool isFacingRight = true;
+    private uint score = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bonus"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.CompareTag("Finish"))
+        {
+            Debug.Log("You won! Score: " + score);
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        if (isFacingRight) Debug.Log("Moved right");
+        else if (!isFacingRight) Debug.Log("Moved left");
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        Debug.Log("Flip");
+    }
 
     bool IsGrounded()
     {
-        return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
+        //return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
+        Vector2 leftEdge = new Vector2(transform.position.x - horizontalRayOffset, transform.position.y);
+        Vector2 rightEdge = new Vector2(transform.position.x + horizontalRayOffset, transform.position.y);
+
+        RaycastHit2D hitLeft = Physics2D.Raycast(leftEdge, Vector2.down, rayLength, groundLayer.value);
+        RaycastHit2D hitRight = Physics2D.Raycast(rightEdge, Vector2.down, rayLength, groundLayer.value);
+
+        // Check if either of the edge rays hit the ground
+        return hitLeft || hitRight;
     }
 
     void Jump()
@@ -51,15 +88,15 @@ public class PLayerController : MonoBehaviour
         { }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
+            if (!isFacingRight) Flip();
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
             isWalking = true;
-            Debug.Log("Moved right");
         }
         else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            if (isFacingRight) Flip();
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
             isWalking = true;
-            Debug.Log("Moved left");
         }
         else
         {
@@ -71,8 +108,18 @@ public class PLayerController : MonoBehaviour
             Jump();
         }
 
+        if (!IsGrounded() && GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            isFalling = true;
+        }
+        else
+        {
+            isFalling = false;
+        }
+
         // Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1, false);
         animator.SetBool("isGrounded", IsGrounded());
         animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isFalling", isFalling);
     }
 }
